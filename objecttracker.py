b61629cd6c7818e609bafeb058eae6758d3d753e
moitrayanN <146983564+moitrayanN@users.noreply.github.com>
2024-10-24 01:50:27 +0530

    Add files via upload
    first commit

---
+++ objecttracker.py
@@ -0,0 +1,30 @@
+from utils import *
+import cv2
+ 
+w,h = 360,240
+pid_yaw = [0.4,0.4,0]
+pid_fb = [1,1,0]
+pError = 0
+startCounter = 0  # for no Flight 1   - for flight 0
+ 
+ 
+myDrone = initializeTello()
+ 
+while True:
+ 
+    ## Flight
+    if startCounter == 0:
+        myDrone.takeoff()
+        startCounter = 1
+ 
+    ## Step 1
+    img = telloGetFrame(myDrone,w,h)
+    ## Step 2
+    img, info = findFace(img)
+    ## Step 3
+    pError = trackFace(myDrone,info,w,pid_yaw,pError)
+    #print(info[0][0])
+    cv2.imshow('Image',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        myDrone.land()
+        break
